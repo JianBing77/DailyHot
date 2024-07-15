4e041f6baebd3a9f8819ae9d0f5c1d58d25a7e23
JianBing77 <82071148+JianBing77@users.noreply.github.com>
2024-07-15 16:45:41 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <n-card :bordered="false" class="header" content-style="padding: 0">
     <section>
       <div class="logo" @click="router.push('/')">
-        <img src="/ico/favicon.png" alt="logo" />
+        <img src="/ico/meow.ico" alt="logo" />
         <div class="name">
           <n-text>神马值得看</n-text>
           <n-text :depth="3">雅俗共赏</n-text>
